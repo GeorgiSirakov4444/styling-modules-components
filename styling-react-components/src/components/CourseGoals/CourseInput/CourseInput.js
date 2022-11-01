11e5d847e40533aff8ea22fcb44472f41e3687c9
Georgi Sirakov <georgi.sirakov74@gmail.com>
2022-11-01 15:40:29 +0200

    modules

--- styling-react-components/src/components/CourseGoals/CourseInput/CourseInput.js
+++ styling-react-components/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -17,16 +17,15 @@ const CourseInput = props => {
       setIsValid(false);
       return;
     }
-    props.onAddGoal(enteredValue);
     setIsValid(true);
+    props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label style={{color: !isValid ? 'red' : 'black'}}>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} 
-        style={{bordercolor: !isValid ? 'red' : 'black', background: !isValid ? 'salmon' : 'transparent'}}/>
+      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+        <label>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler}/> 
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
